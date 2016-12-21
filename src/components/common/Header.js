5e6f5519328b81a8235bee49408279d162bfeea5
Ain <ain.salula@gmail.com>
2016-12-21 23:43:29 +0200

    LoadingDots

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
       <Link to="/courses" activeClassName="active">Courses</Link>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
+      <LoadingDots interval={100} dots={20}/>
     </nav>
   );
 };
