5b761770096cc0436c6aea25ae9ec1e6f759d7de
Ain <ain.salula@gmail.com>
2016-12-24 14:04:15 +0200

    configure production redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/*eslint-disable import/default */
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import configureStore from './store/configureStore';
+import configureStore from './store/configureStore.dev';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
